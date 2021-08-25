1c09cb61af10a4e1316d2a79c8fcdec7e4b25c6b
Yevhen Vasylevytskyi <spirit.ua87@gmail.com>
2021-08-25 08:18:46 +0300

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import GalleriesApiService from './apiService';
 import cardTpl from './templates/card.hbs';
-import '@pnotify/core/dist/BrightTheme.css';
 import "@pnotify/core/dist/PNotify.css";
 import "@pnotify/core/dist/BrightTheme.css";
 import { error, defaultModules, } from "@pnotify/core";
@@ -26,16 +25,17 @@ function onSearch(event) {
     galleriesApiService.fetchArticles().then(data => {
         errorResult(data);
     });
-    galleriesApiService.fetchArticles().then(appendCardsMarkup);
+    // galleriesApiService.fetchArticles().then(appendCardsMarkup);
 };
 
 function errorResult(data) {    
-        if (data.length === 0) {
-            error({
-                text: 'Image not found',
-                delay: 2000
-            });
+    if (data.length === 0) {
+        error({
+            text: 'Image not found',
+            delay: 2000
+        });
     }
+    galleriesApiService.fetchArticles().then(appendCardsMarkup);
     return;
 };
 
